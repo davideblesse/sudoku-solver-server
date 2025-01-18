62e0af7fb60de77d5d405cda995c78009bdea543
Davide Benedetti Valentini <davidebenedettivalentini@MacBook-Pro-di-Davide.local>
2025-01-18 12:35:24 +0100

    Test to see if Render automatically deployes .2

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Sudoku App Automatically deployed!"}
+    return {"message": "Sudoku App Automatically deployed!!!!!"}

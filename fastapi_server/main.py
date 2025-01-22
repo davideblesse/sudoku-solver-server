c414fdded771d3b8c88f2132231b14ae06b7b484
Davide Benedetti Valentini <davidebenedettivalentini@MacBook-Pro-di-Davide.local>
2025-01-23 00:25:49 +0100

    Corrected a bug regarding the image being in binary form and not properly recognized by preprocessing file

--- fastapi_server/main.py
+++ fastapi_server/main.py
@@ -1,4 +1,4 @@
-import io
+import tempfile
 from fastapi import FastAPI, File, UploadFile
 from preprocessing.recognize_digits import recognize_digits
 from preprocessing.sudoku_preprocessing import process_sudoku_image
@@ -9,9 +9,16 @@ app = FastAPI()
 async def process_image(file: UploadFile = File(...)):
     # Read the file into memory
     file_bytes = await file.read()
-    
-    # Generate a fake Sudoku solution with 81 numbers
-    processed_cells = process_sudoku_image(file_bytes)
+
+    # Write the file bytes to a temporary file
+    with tempfile.NamedTemporaryFile(delete=True, suffix=".jpg") as temp_file:
+        temp_file.write(file_bytes)
+        temp_file.flush()  # Ensure all data is written to disk
+
+        # Pass the temporary file path to the preprocessing function
+        processed_cells = process_sudoku_image(temp_file.name)
+
+    # Recognize digits from the processed cells
     recognized_digits = recognize_digits(processed_cells)
     solution_str = ",".join(map(str, recognized_digits))
 
